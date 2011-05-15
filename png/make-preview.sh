fb95a5837040ede8745ca500296a6382310a2e10
Martin Povolny <martin.povolny@solnet.cz>
2011-05-15 17:27:38 +0200

    * added rendered symbols in png as samples

---
+++ png/make-preview.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# render symbols to PNG
+
+echo 'this must be run inside GRASS session'
+
+export GRASS_WIDTH=50
+export GRASS_HEIGHT=50 
+
+for i in $(cd ..; ls | grep -v png) ; do 
+  export GRASS_PNGFILE=$i.png
+  
+  d.mon start=PNG
+  echo "symbol ujc/$i 40 50 50 black black" | d.graph
+  d.mon stop=PNG 
+done
